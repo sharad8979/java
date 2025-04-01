7d2f2b86e8f3e781bac04fb6a285c820e151c48b
sharad8979 <sharadsharma8439@gmail.com>
2025-04-01 06:00:47 +0530

    Add files via upload

---
+++ sharad/eight.java
@@ -0,0 +1,12 @@
+public class eight {
+    public static void main(String[] args) {
+        int a=7;
+        System.out.println("a="+a);
+        System.out.println("a++="+(a++));
+        System.out.println("a after post increment="+a);
+        System.out.println("++a="+(++a));
+        System.out.println("--a="+(--a));
+        System.out.println("a--="+(a--));
+        System.out.println("a after post decrement="+a);
+    }
+}
